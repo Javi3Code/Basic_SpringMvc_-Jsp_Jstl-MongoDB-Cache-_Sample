50bd997a7419655e5428b043595c5d0899350184
@JeyCode <j.code.initialize@gmail.com>
2021-07-01 19:32:39 +0200

    spring intializr proyect

---
+++ src/main/java/org/jeycode/mvcsample/BasicJspJstlSampleApplication.java
@@ -0,0 +1,13 @@
+package org.jeycode.mvcsample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicJspJstlSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasicJspJstlSampleApplication.class, args);
+	}
+
+}
