dd2963f3e1d2234f193fcfb338f6f1388db64e87
@JeyCode <j.code.initialize@gmail.com>
2021-07-06 18:45:33 +0200

    Antes de crear elementos en el segundo form añadimos mongo db

--- src/main/java/org/jeycode/mvcsample/controller/FormController.java
+++ src/main/java/org/jeycode/mvcsample/controller/FormController.java
@@ -6,7 +6,6 @@ import java.util.Set;
 import org.jeycode.mvcsample.model.Student;
 import org.jeycode.mvcsample.model.Teacher;
 import org.springframework.stereotype.Controller;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -22,7 +21,7 @@ public class FormController implements FakeHelper
 
       @GetMapping("/firstSample")
       public String formPage(@ModelAttribute(binding = true, name = "teacher") Teacher teacher,
-                             @ModelAttribute(binding = true, name = "student") Student student,Locale locale,BindingResult result)
+                             @ModelAttribute(binding = true, name = "student") Student student,Locale locale)
       {
             setTeacherFakeValues(teacher);
             return "formPage";
